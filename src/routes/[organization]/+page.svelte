c471476511a8fb87006b58fe47ea4b7574118cd6
Douglas G. Moore <doug@39alpharesearch.org>
2023-11-17 22:06:19 -0700

    Rework the organization pages

--- src/routes/[organization]/+page.svelte
+++ src/routes/[organization]/+page.svelte
@@ -1,80 +1,76 @@
 <script lang="ts">
-    import ListHeader from '$lib/components/ListHeader.svelte';
+    import Sidebar from '$lib/components/Sidebar.svelte';
+
     export let data;
+
+    const avatar = 'https://gateway.39alpharesearch.org/ipfs/QmZJEpyUESuu45zf9JaRZUvnKomg2y5cSkHLC2u1rjcL3Z';
 </script>
 
-<ListHeader href="/">
-    {data.organization}'s Datasets
-</ListHeader>
+<Sidebar organizations={data.organizations} organization={data?.organization} />
 
-<div class="description">
-    <h1>Walkthrough:</h1>
-
-    <p>
-        The next level down from an organization is a <em>dataset</em>. An organization can have any
-        number of datasets. Go ahead and select one of the datasets below, or create a new one.
-    </p>
-</div>
-
-<div>
-    <ul>
-        <li>
-            <a href="/{data.organization}/create">New Dataset</a>
-        </li>
-        {#each data.datasets as { name, path }}
-            <li><a href={path}>{name}</a></li>
+<div class="body">
+    <h1>{data?.organization?.name} Datasets</h1>
+    <div class="body_content">
+    {#if data?.organization?.datasets?.length}
+        {#each data.organization.datasets as { name, path }, i}
+            <div class="dataset">
+                <img class="dataset_avatar" src="{data?.organization?.avatar ?? avatar}" alt="{name} avatar" />
+                <div class="dataset_details">
+                    <div class="dataset_header">
+                        <a href={path}>{data?.organization?.name}/{name}</a>
+                    </div>
+                    <div class="dataset_description">
+                        <p>{data?.organization?.description ?? 'A glorious dataset as yet undescribed'}</p>
+                    </div>
+                </div>
+            </div>
         {/each}
-    </ul>
+    {:else}
+        <p>Organization has no datasets yet.</p>
+    {/if}
+    </div>
 </div>
 
 <style lang="scss">
     @use '$lib/styles/variables' as *;
 
-    h1 {
-        text-align: left;
-        font-size: calc(1.5 * $base-font-size);
-        margin-bottom: calc($spacing-unit / 2);
-    }
+    .body {
+        width: 67%;
 
-    .description {
-        margin-bottom: $spacing-unit;
-        padding: calc($spacing-unit / 2) $spacing-unit;
-        border-top: 1px solid black;
-        border-bottom: 1px solid black;
-
-        h1 {
-            font-size: calc(1.25 * $base-font-size);
-        }
-
-        p {
-            text-align: justify;
-            line-height: calc($base-line-height / 1);
-            margin-bottom: calc($spacing-unit / 2);
-        }
-
-        p:last-child {
-            margin-bottom: 0px;
+        &_content {
+            display: flex;
+            flex-direction: column;
         }
     }
 
-    div {
-        ul {
-            list-style-type: none;
-            margin: 0;
+    .dataset {
+        display: flex;
+        padding: 0;
+        margin: calc($spacing-unit / 2) 0;
+        border: 1px solid #444444;
+        background: $grey-color-10;
+        gap: $spacing-unit;
 
-            li:first-child {
-                margin-bottom: calc($spacing-unit / 2);
-                a {
-                    background: $grey-color-10;
-                    font-weight: bold;
-                    border: 1px solid black;
-                    border-radius: 3px;
-                    padding: calc($spacing-unit / 4);
-                }
+        &_avatar {
+            width: calc(3 * $base-line-height * 1em);
+            height: calc(3 * $base-line-height * 1em);
+        }
 
-                a:hover {
-                    text-decoration: none;
-                }
+        &_details {
+            overflow: hidden;
+            padding: 0 $spacing-unit calc($spacing-unit / 2) 0;
+        }
+
+        &_header {
+            font-weight: bold;
+            font-size: 1.5em;
+        }
+
+        &_description {
+            &, & > * {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
             }
         }
     }
--- src/routes/[organization]/+page.ts
+++ src/routes/[organization]/+page.ts
@@ -1,7 +1,15 @@
 /** @type {import("./$types").PageLoad} */
 export async function load({ fetch, params }) {
-    const res = await fetch(`http://localhost:4248/v0/organization/${params.organization}/dataset`);
-    const datasets = await res.json();
+    try {
+        let res = await fetch(`http://localhost:4248/v0/organization/${params.organization}`);
+        const organization = await res.json();
 
-    return { ...params, datasets };
+        res = await fetch(`http://localhost:4248/v0/organization${organization.path}/dataset`);
+        organization.datasets = await res.json();
+
+        return { organization };
+    } catch (err) {
+        console.error(err);
+        return {};
+    }
 }
